223858ca0fd9294e553bc26fd34074233ddc801b
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-03 00:36:56 +1300

    add minidumps

--- init.lua
+++ init.lua
@@ -8,6 +8,13 @@ typedef int DWORD;
 typedef short WORD;
 typedef void *LPVOID;
 typedef int *DWORD_PTR;
+typedef void *HANDLE;
+typedef void *PEXCEPTION_POINTERS;
+typedef int BOOL;
+typedef unsigned UINT;
+
+typedef long (__stdcall *PTOP_LEVEL_EXCEPTION_FILTER)(PEXCEPTION_POINTERS);
+PTOP_LEVEL_EXCEPTION_FILTER SetUnhandledExceptionFilter(PTOP_LEVEL_EXCEPTION_FILTER lpTopLevelExceptionFilter);
 
 typedef struct _SYSTEM_INFO {
 	union {
@@ -28,6 +35,36 @@ typedef struct _SYSTEM_INFO {
 	WORD wProcessorRevision;
 } SYSTEM_INFO, *LPSYSTEM_INFO;
 
+typedef enum _MINIDUMP_TYPE {
+	MiniDumpNormal = 0x00000000,
+	MiniDumpWithDataSegs = 0x00000001,
+	MiniDumpWithFullMemory = 0x00000002,
+	MiniDumpWithHandleData = 0x00000004,
+	MiniDumpFilterMemory = 0x00000008,
+	MiniDumpScanMemory = 0x00000010,
+	MiniDumpWithUnloadedModules = 0x00000020,
+	MiniDumpWithIndirectlyReferencedMemory = 0x00000040,
+	MiniDumpFilterModulePaths = 0x00000080,
+	MiniDumpWithProcessThreadData = 0x00000100,
+	MiniDumpWithPrivateReadWriteMemory = 0x00000200,
+	MiniDumpWithoutOptionalData = 0x00000400,
+	MiniDumpWithFullMemoryInfo = 0x00000800,
+	MiniDumpWithThreadInfo = 0x00001000,
+	MiniDumpWithCodeSegs = 0x00002000,
+	MiniDumpWithoutAuxiliaryState = 0x00004000,
+	MiniDumpWithFullAuxiliaryState = 0x00008000,
+	MiniDumpWithPrivateWriteCopyMemory = 0x00010000,
+	MiniDumpIgnoreInaccessibleMemory = 0x00020000,
+	MiniDumpWithTokenInformation = 0x00040000,
+	MiniDumpWithModuleHeaders = 0x00080000,
+	MiniDumpFilterTriage = 0x00100000,
+	MiniDumpWithAvxXStateContext = 0x00200000,
+	MiniDumpWithIptTrace = 0x00400000,
+	MiniDumpScanInaccessiblePartialPages = 0x00800000,
+	MiniDumpFilterWriteCombinedMemory,
+	MiniDumpValidTypeFlags = 0x01ffffff
+} MINIDUMP_TYPE;
+
 bool VirtualProtect(void *adress, size_t size, int new_protect, int* old_protect);
 void *VirtualAlloc(void* lpAddress, size_t dwSize, uint32_t flAllocationType, uint32_t flProtect);
 void GetSystemInfo(LPSYSTEM_INFO lpSystemInfo);
@@ -41,6 +78,14 @@ void *GetModuleHandleA(const char* lpModuleName);
 void *GetProcAddress(void* hModule, const char* lpProcName);
 
 LPVOID TlsGetValue(DWORD dwTlsIndex);
+
+HANDLE CreateFileA(const char *lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, void *lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile);
+BOOL MiniDumpWriteDump(HANDLE hProcess, DWORD ProcessId, HANDLE hFile, MINIDUMP_TYPE DumpType, PEXCEPTION_POINTERS ExceptionParam, void *UserStreamParam, void *CallbackParam);
+HANDLE GetCurrentProcess();
+DWORD GetCurrentProcessId();
+DWORD GetLastError();
+void ExitProcess(UINT uExitCode);
+BOOL TerminateProcess(HANDLE hProcess, UINT uExitCode);
 ]])
 
 local info = ffi.new("SYSTEM_INFO")
@@ -49,21 +94,63 @@ ffi.C.GetSystemInfo(info)
 local page_size = info.dwPageSize
 
 _G["hello"] = function()
-	print("we are called")
+	print("we are called\n") -- if we print too much the logs buffer will overflow and we die
+	while true do
+	end
 end -- this is the cell hook function
+
+local dbghelp = ffi.load("DbgHelp.dll")
+local kernel32 = ffi.load("kernel32.dll")
+local function cs(str)
+	return ffi.new("char[?]", #str + 1, str)
+end
+local lua = kernel32.GetModuleHandleA(cs("lua51.dll"))
+local get_value = ffi.cast("char *", kernel32.TlsGetValue)
+local get_field = kernel32.GetProcAddress(lua, cs("lua_getfield"))
+local call = kernel32.GetProcAddress(lua, cs("lua_call"))
+
+local function create_crash_dump(exception_info, dump_path)
+	local file = ffi.C.CreateFileA(
+		dump_path,
+		0x40000000, -- GENERIC_WRITE
+		0, -- No sharing
+		nil,
+		2, -- CREATE_ALWAYS
+		0x80, -- FILE_ATTRIBUTE_NORMAL
+		nil
+	)
+
+	dbghelp.MiniDumpWriteDump(
+		ffi.C.GetCurrentProcess(),
+		ffi.C.GetCurrentProcessId(),
+		file,
+		0x0, -- perhaps change this to dump more info
+		exception_info,
+		nil,
+		nil
+	)
+end
 
 M.post = function(api, config)
 	local old_creature_list = creature_list
 	creature_list = function(...)
-		local kernel32 = ffi.load("kernel32.dll")
-		local function cs(str)
-			return ffi.new("char[?]", #str + 1, str)
+		local already_crash = false
+
+		local function exception_handler(exception_info) -- do this only on 1 thread
+			if already_crash then
+				kernel32.TerminateProcess(kernel32.GetCurrentProcess(), 1) -- if we aren't closing just murder the process
+				-- this still doesn't work on other threads :sob:
+				return 0
+			end
+			already_crash = true -- hax to make the exception handler exiting not trigger the exception handler
+			local crash_filename = os.date("./dumps/Lua_Modloader_%Y_%m_%d_%H_%M_%S.dmp") -- crashes :angry:
+			create_crash_dump(exception_info, crash_filename)
+			kernel32.ExitProcess(1) -- it doesnt seem to like dying
+			return 0
 		end
-		local get_value = ffi.cast("char *", kernel32.TlsGetValue)
-		local lua = kernel32.GetModuleHandleA(cs("lua51.dll"))
-		local get_field = kernel32.GetProcAddress(lua, cs("lua_getfield"))
-		local pcall = kernel32.GetProcAddress(lua, cs("lua_pcall"))
-		print("TLS: ", get_value, "\n")
+
+		local exception_handler_c = ffi.cast("PTOP_LEVEL_EXCEPTION_FILTER", exception_handler)
+		ffi.C.SetUnhandledExceptionFilter(exception_handler_c)
 
 		---@type Patch
 		local patch = {
@@ -107,12 +194,12 @@ M.post = function(api, config)
 		--stylua: ignore start
 		local data = {
 			0x48, 0xB9, 0x14, 0x74, 0x1D, 0x40, 0x01, 0x00, 0x00, 0x00, 0x8B, 0x09, 0x48, 0xB8,
-			0x10, 0x32, 0x54, 0x76, 0x98, 0xCA, 0xDB, 0xFE,
+			0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
 			0xFF, 0xD0, 0x49, 0xB8,
 			0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
 			0xBA, 0xEE, 0xD8, 0xFF, 0xFF, 0x4C, 0x8B, 0x60, 0x18, 0x4C, 0x89, 0xE1, 0x48, 0xB8,
 			0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
-			0xFF, 0xD0, 0x4C, 0x89, 0xE1, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x41, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x41, 0xB9, 0x00, 0x00, 0x00, 0x00, 0x48, 0xB8,
+			0xFF, 0xD0, 0x4C, 0x89, 0xE1, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x41, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x48, 0xB8,
 			0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
 			0xFF, 0xD0, 0xC3
 		}
@@ -145,7 +232,7 @@ M.post = function(api, config)
 		pointer_at_addr(get_value, 15)
 		pointer_at_addr(str, 27)
 		pointer_at_addr(get_field, 49)
-		pointer_at_addr(pcall, 81)
+		pointer_at_addr(call, 75)
 		for k, v in ipairs(data) do
 			new_fn[k - 1] = v
 		end
